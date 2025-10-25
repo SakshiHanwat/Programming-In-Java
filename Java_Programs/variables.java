8a9864a27cf1102e299aed058e1dd3574a3910f1
Sakshi Hanwat <hanwatsakshi900@gmail.com>
2025-10-25 07:30:17 +0000

    variables

---
+++ Java_Programs/variables.java
@@ -0,0 +1,13 @@
+public class variables{
+    public static void main(String[]args){
+        int a = 10;
+        String name = "Tony Stark";
+
+        System.out.println("a= "+a);
+        System.out.println("name= "+name);
+
+        //we also change variables value (reassign value)
+        a = 50;
+        System.out.println("a= "+a);
+    }
+}
