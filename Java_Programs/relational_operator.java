ea8d0659c423592ee28b6fda13c9f18fb5e85dbe
Sakshi Hanwat <hanwatsakshi900@gmail.com>
2025-10-25 09:50:45 +0000

    operator

---
+++ Java_Programs/relational_operator.java
@@ -0,0 +1,13 @@
+public class relational_operator {
+    public static void main(String[] args) {
+         int a = 10;
+        int b = 20; //pre increment
+        System.out.println(a == b);
+        System.out.println(a != b);
+        System.out.println(a > b);
+        System.out.println(a < b);
+        System.out.println(a >= b);
+        System.out.println(a <= b);
+    }
+           
+}
