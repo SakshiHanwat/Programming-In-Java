b541bb353d70d52f340b531500660702f42b1eb3
Sakshi Hanwat <hanwatsakshi900@gmail.com>
2025-10-25 07:43:02 +0000

    datatypes

---
+++ Java_Programs/datatypes.java
@@ -0,0 +1,18 @@
+public class datatypes{
+    public static void main(String[]args){
+        byte b = 8;
+        char ch = 'a';
+        boolean var = false;
+        float price = 10.5f;
+        int number = 25;
+        //long
+        //double
+        short n = 240;
+        System.out.println(b);
+        System.out.println(ch);
+        System.out.println(var);
+        System.out.println(price);
+        System.out.println(number);
+        System.out.println(n);
+    }
+}
