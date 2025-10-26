4c5f73665668aa46c4de16ce2ff28aa1beb814e1
Sakshi Hanwat <hanwatsakshi900@gmail.com>
2025-10-26 08:44:07 +0000

    loop

---
+++ Java_Programs/While_loop.java
@@ -0,0 +1,10 @@
+public class While_loop{
+    public static void main(String[]args){
+        int counter = 0;
+        while(counter <100){
+            System.out.println("Hello World");
+            counter++;
+        }
+        System.out.println("Printed 100 times Hello World..");
+    }
+}
