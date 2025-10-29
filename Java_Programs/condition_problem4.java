a067c1d6bc666e9eb1eb3b4be1d93a65c9b43042
Sakshi Hanwat <hanwatsakshi900@gmail.com>
2025-10-29 16:51:22 +0000

    condition problem 4

---
+++ Java_Programs/condition_problem4.java
@@ -0,0 +1,14 @@
+public class condition_problem4 {
+
+    public static void main(String args[]) {
+        int a = 63, b = 36;
+
+        boolean x = (a < b) ? true : false;
+
+        int y = (a > b) ? a : b;
+
+        System.out.println("Value of x = " + x);
+                System.out.println("Value of y = "+y);
+
+    }
+}
