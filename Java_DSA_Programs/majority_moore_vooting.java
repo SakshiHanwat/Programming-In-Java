419ce9167955226936575a8e03a9826828b01a47
Sakshi Hanwat <hanwatsakshi900@gmail.com>
2025-11-12 10:44:18 +0530

    Create majority_moore_vooting.java

---
+++ Java_DSA_Programs/majority_moore_vooting.java
@@ -0,0 +1,28 @@
+public class MajorityElement {
+    public static int majorityElement(int[] nums) {
+        int freq = 0;   // frequency counter
+        int ans = 0;    // potential majority element
+
+        for (int i = 0; i < nums.length; i++) {
+            if (freq == 0) {
+                ans = nums[i];   // choose current element as candidate
+            }
+
+            if (ans == nums[i]) {
+                freq++;          // same as candidate → increase count
+            } else {
+                freq--;          // different element → decrease count
+            }
+        }
+
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int[] nums1 = {3, 2, 3};
+        int[] nums2 = {2, 2, 1, 1, 1, 2, 2};
+
+        System.out.println("Example 1 Output: " + majorityElement(nums1)); // Output: 3
+        System.out.println("Example 2 Output: " + majorityElement(nums2)); // Output: 2
+    }
+}
